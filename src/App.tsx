caf2f62921c02d5df3681e5ab8f2b679da52bd1a
Jaqueline Costa <jaqueline.costa@eleevait.com.br>
2022-12-20 15:26:14 -0300

    "first commit"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [list, setList] = useState<any>([]);
+  const [undid, setUndid] = useState<any>([]);
+
+  const handleClick = (e: any) => {
+    const newCircle = {
+      clientX: e.clientX,
+      clientY: e.clientY,
+    };
+    setList([...list, newCircle]);
+  };
+
+  const handleContainerButton = (event: any) => {
+    event.stopPropagation();
+  };
+
+  const handleUndo = () => {
+    const newArray = [...list].slice(0, -1);
+    if (list.length === 0) {
+      return;
+    }
+    const lastItem = list[list.length -1];
+    setUndid([...undid, lastItem]);
+    setList(newArray);
+  };
+
+  const handleRedo = () => {
+    const recoverLastItem = undid[undid.length -1];
+    const newArray = [...undid].slice(0, -1);
+    if (undid.length === 0) {
+      return;
+    }
+    setUndid(newArray);
+    setList([...list, recoverLastItem]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" onClick={handleClick}>
+      <div className="container-button" onClick={handleContainerButton}>
+        <button className="btn-undo" onClick={handleUndo}>
+          Desfazer
+        </button>
+        <button className="btn-redo" onClick={handleRedo}>
+          Refazer
+        </button>
+      </div>
+      {list.map((item, i) => {
+        return (
+          <span
+            key={i}
+            className="circle"
+            style={{ left: item.clientX, top: item.clientY }}
+          />
+        );
+      })}
     </div>
   );
 }
-
-export default App;
